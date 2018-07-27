7e6b1d22fa8dce997973b9d2754109d94280168d
lindsay1623 <40032374+lindsay1623@users.noreply.github.com>
2018-07-27 10:24:01 -0500

    update stripping_names.py

--- stripping_names.py
+++ stripping_names.py
@@ -1,5 +1,5 @@
-name_1 = " Kaley Nelson "
-name_2 = " Jacob Smith "
+name_1 = " Kaley Nelson \t"
+name_2 = " Jacob Smith \n"
 
 print("Current Names: \n\t" + name_1.strip() + "\n\t" + name_2.strip())
 print(name_1)
